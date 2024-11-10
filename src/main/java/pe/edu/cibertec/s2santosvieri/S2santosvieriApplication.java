05485648995e88303bc05e5c8d2ba0d9a33811ee
AngelSantos2912 <angelleonardo975533406@gmail.com>
2024-11-10 09:56:41 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/s2santosvieri/S2santosvieriApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.s2santosvieri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2santosvieriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2santosvieriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/s2santosvieri/S2santosvieriApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.s2santosvieri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2santosvieriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
